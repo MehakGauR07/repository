8f5c28907050b82071ecfed6810bf49f41aa3117
Mehak Gaur <97112626+MehakGauR07@users.noreply.github.com>
2023-12-10 16:53:36 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,12 @@
+#Calculator
+x1=input("Enter first number: ")
+y1=input("Enter second number: ")
+print("sum of x and y is:" , int(x1)+int(y1) )
+print("subtraction of x and y is:" , int(x1)-int(y1) )
+print("multipication of x and y is:" , int(x1)*int(y1) )
+print("division of x and y is:" , int(x1)/int(y1) )
+print("floor division of x and y is:" , int(x1)//int(y1) )
+print("exponential of x and y is:" , int(x1)**int(y1) )
+print("modulus of x and y is:" , int(x1)%int(y1) )
+#needs typcasting cz everything in input command is str
+
